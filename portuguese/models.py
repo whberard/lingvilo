81dfb944160fe643127aded5150f9ebe1c8e2e18
W Berard <wberard@math.arizona.edu>
2017-05-31 10:25:47 -0700

    Add a Verb model.

--- portuguese/models.py
+++ portuguese/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Verb(models.Model):
+    verb = models.CharField(max_length=30)
+    translation = models.CharField(max_length=50, blank=True)
+    is_transitive = models.BooleanField()
+
+    def __str__(self):
+        return self.verb
