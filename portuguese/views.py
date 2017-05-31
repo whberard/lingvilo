c232df97584c321a3e4ed54bf5349e0a1b431b3f
W Berard <wberard@math.arizona.edu>
2017-05-30 22:26:08 -0700

    Add -ir and -er conjugations to the possibilities.

--- portuguese/views.py
+++ portuguese/views.py
@@ -3,16 +3,43 @@ from django.shortcuts import render
 # Create your views here.
 def conjugate_pt(request):
     if 'verb' in request.GET and request.GET['verb']:
+        cando = False
         verb = request.GET['verb']
         ending = verb[-2:]
         root = verb[:-2]
-        present = {'eu': root + 'o',
-                'tu': root + 'as',
-                'ela': root + 'a',
-                'nós': root + 'amos',
-                'vós': root + 'ais',
-                }
-        return render(request, 'conjugate.html', {'verb': verb, 'ending': ending, 'root': root, 'present': present})
+        if ending == "ar":
+            cando = True
+            present = {'eu': root + 'o',
+                    'tu': root + 'as',
+                    'ela': root + 'a',
+                    'nós': root + 'amos',
+                    'elas': root + 'am',
+                    }
 
-    return render(request, 'conjugate.html', {})
+        elif ending == "er":
+            cando = True
+            present = {'eu': root + 'o',
+                    'tu': root + 'es',
+                    'ela': root + 'e',
+                    'nós': root + 'emos',
+                    'elas': root + 'em'
+                    }
+
+        elif ending == "ir":
+            cando = True
+            present = {'eu': root + 'o',
+                    'tu': root + 'es',
+                    'ela': root + 'e',
+                    'nós': root + 'imos',
+                    'elas': root+ 'em'
+                    }
+
+        if cando:
+            return render(request, 'conjugate.html', {'verb': verb, 'ending': ending, 'root': root, 'present': present})
+        else: 
+            errormsg = "I only know how to conjugate regular verbs that end in -ar, -er, or -ir."
+            return render(request, 'conjugate.html', {'verb': verb, 'error': errormsg})
+
+    else: 
+        return render(request, 'conjugate.html', {})
 
