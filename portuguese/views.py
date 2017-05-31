c966a7d900e79e48565178b08a71d432cff47372
W Berard <wberard@math.arizona.edu>
2017-05-30 21:58:36 -0700

    Add simple present tense conjugation to the view.

--- portuguese/views.py
+++ portuguese/views.py
@@ -2,5 +2,17 @@ from django.shortcuts import render
 
 # Create your views here.
 def conjugate_pt(request):
+    if 'verb' in request.GET and request.GET['verb']:
+        verb = request.GET['verb']
+        ending = verb[-2:]
+        root = verb[:-2]
+        present = {'eu': root + 'o',
+                'tu': root + 'as',
+                'ela': root + 'a',
+                'nós': root + 'amos',
+                'vós': root + 'ais',
+                }
+        return render(request, 'conjugate.html', {'verb': verb, 'ending': ending, 'root': root, 'present': present})
+
     return render(request, 'conjugate.html', {})
 
