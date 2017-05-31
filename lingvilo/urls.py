8786b3c5835091cf2fb100e6427e31aea08e67bf
W Berard <wberard@math.arizona.edu>
2017-05-30 21:16:56 -0700

    Fix the location of conjugate_pt.

--- lingvilo/urls.py
+++ lingvilo/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from lingvilo.views import conjugate_pt
+from portuguese.views import conjugate_pt
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
