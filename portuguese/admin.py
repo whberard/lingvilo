3cef98e13a718ab25f5fe2ef76a454b63b6614bb
W Berard <wberard@math.arizona.edu>
2017-06-19 08:07:26 -0700

    Remove some models. Gonna use nltk instead.

--- portuguese/admin.py
+++ portuguese/admin.py
@@ -1,12 +1,3 @@
 from django.contrib import admin
-from .models import Verb, Reason, End, Start
+from .models import Verb
 
-class StartAdmin(admin.ModelAdmin):
-    list_display = ('phrase', 'translation','why')
-    filter_horizontal = ('ends',)
-
-
-admin.site.register(Verb)
-admin.site.register(Reason)
-admin.site.register(End)
-admin.site.register(Start, StartAdmin)
