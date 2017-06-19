6e22d1d0ea763a4f4db1aa02f7a28f661ba6f8ff
W Berard <wberard@math.arizona.edu>
2017-06-18 21:32:45 -0700

    Add Start, End, Reason models for subjunctive stuff.

--- portuguese/models.py
+++ portuguese/models.py
@@ -8,3 +8,29 @@ class Verb(models.Model):
 
     def __str__(self):
         return self.verb
+
+
+class Reason(models.Model):
+    reason = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.reason
+
+
+class End(models.Model):
+    phrase = models.CharField(max_length=100)
+    translation = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return self.phrase
+
+
+class Start(models.Model):
+    phrase = models.CharField(max_length=100)
+    translation = models.CharField(max_length=100, blank=True)
+    why = models.ForeignKey(Reason, blank=True, null=True, on_delete=models.SET_NULL)
+    ends = models.ManyToManyField(End, blank=True)
+
+
+    def __str__(self):
+        return self.phrase
