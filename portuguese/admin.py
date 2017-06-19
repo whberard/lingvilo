85f841ec125d216da7820a3c4671dc9235ecbb0e
W Berard <wberard@math.arizona.edu>
2017-06-18 21:31:53 -0700

    Register subjunctive models with the admin page.

--- portuguese/admin.py
+++ portuguese/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
-from .models import Verb
+from .models import Verb, Reason, End, Start
 
-# Register your models here.
+class StartAdmin(admin.ModelAdmin):
+    list_display = ('phrase', 'translation','why')
+    filter_horizontal = ('ends',)
+
+
 admin.site.register(Verb)
+admin.site.register(Reason)
+admin.site.register(End)
+admin.site.register(Start, StartAdmin)
