9ee2ef2e158968aab7ed3f191cf324cffe9e1fa9
W Berard <wberard@math.arizona.edu>
2017-06-25 20:26:50 -0700

    Add WordAdmin class to display words nicely in the admin page.

--- portuguese/admin.py
+++ portuguese/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Verb, Word
 
+class WordAdmin(admin.ModelAdmin):
+    list_display = ('word', 'translation', 'is_known', 'last_review')
+    ordering = ('word',)
+    date_hierarchy = 'last_review'
 
-admin.site.register(Word)
+admin.site.register(Word, WordAdmin)
 
