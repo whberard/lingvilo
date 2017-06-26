6fb81b3497b78bb94d3da775218705367957fce4
W Berard <wberard@math.arizona.edu>
2017-06-25 20:27:43 -0700

    Add a last_review field to each word, to see which words still need to be updated.

--- portuguese/models.py
+++ portuguese/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import datetime
 
 # Create your models here.
 class Verb(models.Model):
@@ -14,6 +15,7 @@ class Word(models.Model):
     word = models.CharField(max_length=30)
     translation = models.CharField(max_length=100, blank=True)
     is_known = models.BooleanField()
+    last_review = models.DateField(default=datetime.date(2000,1,1))
 
     def __str__(self):
         return self.word
